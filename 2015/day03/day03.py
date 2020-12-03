b0e334cbb5ba7f61295ba1258f469b10d433f7f8
Albert Vernon Smith <albertvs@umich.edu>
2020-12-03 16:43:38 -0500

    Cleaned function

--- 2015/day03/day03.py
+++ 2015/day03/day03.py
@@ -9,44 +9,27 @@ houses = [[0,0]]
 santa = [[0,0]]
 robo = [[0,0]]
 
+def deliver_presents(h, m):
+  (ns, ew) = h[-1]
+  if move == '^':
+    ns += 1
+  if move == 'v':
+    ns -= 1
+  if move == '<':
+    ew -= 1
+  if move == '>':
+    ew += 1
+  h.append([ns,ew])
 
 for i in range(len(data)):
   move = data[i]
-  (ns, ew) = houses[-1]
-  (sns, sew) = santa[-1]
-  (rns, rew) = robo[-1]
-  if move == '^':
-    ns += 1
-    if i % 2 == 0:
-      sns += 1
-    else:
-      rns += 1
-  if move == 'v':
-    ns -= 1
-    if i % 2 == 0:
-      sns -= 1
-    else:
-      rns -= 1
-  if move == '<':
-    ew -= 1
-    if i % 2 == 0:
-      sew -= 1
-    else:
-      rew -= 1
-  if move == '>':
-    ew += 1
-    if i % 2 == 0:
-      sew += 1
-    else:
-      rew += 1
-  houses.append([ns,ew])
-  santa.append([sns,sew])
-  robo.append([rns,rew])
-
-
-
+  deliver_presents(houses,move)
+  if i % 2:
+    deliver_presents(santa,move)
+  else:
+    deliver_presents(robo,move)
+ 
 num_houses = len(set([tuple(h) for h in houses]))
 print("Houses year 1:", num_houses)
 num_houses_year2 = len(set([tuple(h) for h in santa + robo]))
-
 print("Houses year 2:", num_houses_year2)
