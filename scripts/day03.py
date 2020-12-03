3eff52b55924e8d493e9c4013489ac342f6e9eb3
Albert Vernon Smith <albertvs@umich.edu>
2020-12-03 11:51:04 -0500

    Cleaning

--- scripts/day03.py
+++ scripts/day03.py
@@ -20,30 +20,26 @@ testdata = '''..##.......
 .#..#...#.#
 '''
 
-offset = 3
-
 orig = data.splitlines()
-length = len(orig[0])
-array = [int(reverse(s.replace('#','1').replace('.','0')),2) for s in orig]
-
-trees = 0 
 
 def trees_hit(slope ,right = 3, down = 1, debug = False):
   trees = 0
   for y in range(0,len(slope),down):
-    x = (y//down * right) % length 
-    pos = slope[y] >> x
-    trees +=  pos&1==1
+    slopeline = slope[y]
+    slopeint = int(reverse(slopeline.replace('#','1').replace('.','0')),2) 
+    x = (y//down * right) % len(slopeline)
+    pos = slopeint >> x
+    trees += pos&1==1
+
     if(debug):
-      line = orig[y]
-      if line[x] == '#':
-        newline = line[:x] + 'X' + line[x + 1:]
+      if slopeline[x] == '#':
+        newline = slopeline[:x] + 'X' + slopeline[x + 1:]
       else:
-        newline = line[:x] + 'O' + line[x + 1:]
-      print(line, newline)
+        newline = slopeline[:x] + 'O' + slopeline[x + 1:]
+      print(slopeline, newline)
   return(trees)
 
-print("Part 1 answer:", trees_hit(array))
-print("Part 2 answer:", trees_hit(array,1,1)* trees_hit(array,3,1) * trees_hit(array,5,1)* trees_hit(array,7,1)* trees_hit(array,1,2))
+print("Part 1 answer:", trees_hit(orig))
+print("Part 2 answer:", trees_hit(orig,1,1)* trees_hit(orig,3,1) * trees_hit(orig,5,1)* trees_hit(orig,7,1)* trees_hit(orig,1,2))
 
 
