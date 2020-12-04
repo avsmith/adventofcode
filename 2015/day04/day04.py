3ffc0a4e2a5db446becdae4e7d99a46736a9825c
Albert Vernon Smith <albertvs@umich.edu>
2020-12-04 14:40:49 -0500

    Day 04 solution

---
+++ 2015/day04/day04.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import hashlib
+import re
+
+inkey = 'yzbqklnj'
+
+def mine_hash(code, num=1, leadzeros=5):
+  hashre = re.compile(rf"^[0]{{{leadzeros},}}")
+  while True:
+    codenum =code + str(num)
+    res = hashlib.md5(codenum.encode())
+    result = res.hexdigest()
+    if hashre.match(result):
+      return(str(num), res.hexdigest())
+    num += 1
+    
+print(mine_hash(inkey))
+print(mine_hash(inkey,leadzeros=6))
