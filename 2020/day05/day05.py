96a5927ba8291eb9714096c4d8dded4294022136
Albert Vernon Smith <albertvs@umich.edu>
2020-12-05 01:42:05 -0500

    day05 solution

---
+++ 2020/day05/day05.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os, sys
+import re
+
+f = open(os.path.join(sys.path[0], 'input05.txt'))
+data = f.read()
+codes = data.splitlines()
+
+def find_seat(txt):
+  row = '0b'
+  col = '0b'
+  for c in txt:
+    if c == 'B':
+      row += '1'
+    elif c == 'F':
+      row += '0'
+    elif c == 'R':
+      col += '1'
+    elif c == 'L':
+      col += '0'
+  return(int(row,2) * 8 + int(col,2))
+
+max_seat = 0
+plane = []
+
+for code in codes:
+  seat_id = find_seat(code)
+  plane.append(seat_id)
+  if seat_id > max_seat:
+    max_seat = seat_id
+  
+print(max_seat)
+print(list(set(list(range(min(plane),max(plane)+1))) - set(plane))[0])
