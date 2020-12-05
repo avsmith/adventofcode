5e218dd0950ad7e0b8205b85aa7e93cf94a330c6
Albert Vernon Smith <albertvs@umich.edu>
2020-12-05 01:43:04 -0500

    formatting

--- 2015/day04/day04.py
+++ 2015/day04/day04.py
@@ -7,13 +7,14 @@ inkey = 'yzbqklnj'
 
 def mine_hash(code, num=1, leadzeros=5):
   hashre = re.compile(rf"^[0]{{{leadzeros},}}")
+
   while True:
-    codenum =code + str(num)
+    codenum = code + str(num)
     res = hashlib.md5(codenum.encode())
     result = res.hexdigest()
     if hashre.match(result):
       return(str(num), res.hexdigest())
     num += 1
-    
+
 print(mine_hash(inkey))
 print(mine_hash(inkey,leadzeros=6))
