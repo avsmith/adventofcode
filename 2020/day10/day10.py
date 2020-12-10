f1386d0aad4a43aa3618e54f0e7ef0bbb6e3f4ce
Albert Vernon Smith <albertvs@umich.edu>
2020-12-10 15:16:41 +0000

    Cleaning names

--- 2020/day10/day10.py
+++ 2020/day10/day10.py
@@ -6,7 +6,7 @@ from itertools import  chain, combinations
 f = open(os.path.join(sys.path[0], 'input10.txt'))
 d = f.read()
 
-srted = [0] + sorted( [int(x) for x in d.splitlines()])
+jolt_input = [0] + sorted( [int(x) for x in d.splitlines()])
 
 
 def powerset(iterable):
@@ -14,57 +14,58 @@ def powerset(iterable):
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
   
-def legal_set_counts(candidates):
-  if(len(candidates)) <= 2:
+def legal_set_counts(input_array_set):
+  if(len(input_array_set)) <= 2:
     set_count = 1
   else:
-    start = candidates.pop(0)
-    end = candidates.pop()
-    pset = powerset(candidates)
+    first_item = input_array_set.pop(0)
+    last_item = input_array_set.pop()
+    possible_subsets = powerset(input_array_set)
     set_count = 0
-    for p in pset:
-      set_count += legal_set([start] + sorted(list(p)) + [end] )
-#  print(set_count)
+    for subset in possible_subsets:
+      set_count += check_legal_set([first_item] + sorted(list(subset)) + [last_item] )
   return(set_count)
 
-def legal_set(values):
-  for i in range(len(values)-1):
-    if values[i+1] - values[i] > 3:
+def check_legal_set(input_set):
+  for i in range(len(input_set)-1):
+    if input_set[i+1] - input_set[i] > 3:
       return False
   return True
 
-def multiplyList(myList) :
-     
+def multiplyList(integer_list) :
     # Multiply elements one by one
     result = 1
-    for x in myList:
+    for x in integer_list:
          result = result * x 
     return result 
 
-cnts = dict()
+step_counts = dict()
 
-srt_splt = []
-tracker = []
-for i in range(len(srted)):
-  if i == 0 or srted[i] - srted[i-1] == 1:
-    tracker.append(srted[i])
-  elif srted[i] - srted[i-1] == 3:
-    srt_splt.append(tracker)
-    tracker = [srted[i]]
-srt_splt.append(tracker)
+jolt_input_split = []
+jolt_subset_by_one = []
+for i in range(len(jolt_input)):
+  if i == 0 or jolt_input[i] - jolt_input[i-1] == 1:
+    jolt_subset_by_one.append(jolt_input[i])
+  elif jolt_input[i] - jolt_input[i-1] == 3:
+    jolt_input_split.append(jolt_subset_by_one)
+    jolt_subset_by_one = [jolt_input[i]]
+
+jolt_input_split.append(jolt_subset_by_one)
 
 
-for i in range(len(srted)-1):
-  dif = srted[i+1]-srted[i]
-  if dif in cnts:
-    cnts[dif] += 1
+for i in range(len(jolt_input)-1):
+  jolt_step = jolt_input[i+1]-jolt_input[i]
+  if jolt_step in step_counts:
+    step_counts[jolt_step] += 1
   else:
-    cnts[dif] = 1
+    step_counts[jolt_step] = 1
 
-print(cnts[1]*(cnts[3]+1))
+print(f"Answer 1: {step_counts[1]*(step_counts[3]+1)}")
 
-legal_sets = [legal_set_counts(x) for x in srt_splt]
-val = 1
-for x in legal_sets:
-  val *= x
-print(val)
+number_legal_sets = [legal_set_counts(x) for x in jolt_input_split]
+
+second_answer = 1
+for x in number_legal_sets:
+  second_answer *= x
+
+print(f"Answer 2: {second_answer}")
