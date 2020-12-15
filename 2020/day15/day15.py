bc8e375f520bbace265b25557c53fd73a31049e9
Albert Vernon Smith <albertvs@umich.edu>
2020-12-15 07:37:33 +0000

    rename variable

--- 2020/day15/day15.py
+++ 2020/day15/day15.py
@@ -4,24 +4,22 @@ input = '1,0,16,5,17,4'
 values = [int(x) for x in input.split(',')]
 count_dict = {}
 
-pos = 0
+turn = 0
 
 for val in values:
-  pos += 1
-  count_dict[val] = {"pos": pos}
+  turn += 1
+  count_dict[val] = {"turn": turn}
 
 next_num = 0
 
-while pos < 30000000:
-  pos += 1
-  if pos == 2020 or pos == 30000000:
-    print(f"Turn: {pos} Number: {next_num}")
+while turn < 30000000:
+  turn += 1
+  if turn == 2020 or turn == 30000000:
+    print(f"Turn: {turn} Number: {next_num}")
   if next_num in count_dict.keys():
-    last_pos = count_dict[next_num]['pos']
-#    print(last_pos, count_dict)
-    
-    count_dict[next_num] = {'pos': pos }
-    next_num = pos - last_pos
+    last_turn = count_dict[next_num]['turn']
+    count_dict[next_num] = {'turn': turn }
+    next_num = turn - last_turn
   else:
-    count_dict[next_num] ={"pos": pos}
+    count_dict[next_num] ={"turn": turn}
     next_num = 0
