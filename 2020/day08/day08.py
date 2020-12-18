c00e209f8084c244833896cd6928649f96f6d4b4
Albert Vernon Smith <albertvs@umich.edu>
2020-12-18 06:11:50 +0000

    day08 solution, part 2

--- 2020/day08/day08.py
+++ 2020/day08/day08.py
@@ -1,29 +1,15 @@
 #!/usr/bin/env python
 
 import os, sys
-import pprint
-
 
 f = open(os.path.join(sys.path[0], 'input08.txt'))
 input = f.read()
 
-testinput = '''nop +0
-acc +1
-jmp +4
-acc +3
-jmp -3
-acc -99
-acc +1
-jmp -4
-acc +6
-'''
-
 tracking = []
 
-for rule in testinput.splitlines():
+for rule in input.splitlines():
   (action, amount) = rule.split(' ')
   tracking.append({'action': action, 'amount': int(amount)})
-
 
 def hand_held(tracker, loop=True):
   pos = 0;
@@ -31,6 +17,10 @@ def hand_held(tracker, loop=True):
   visited = set()
   while True:
     if pos in visited and loop is True:
+      return(acc)
+    elif pos in visited and loop is not True:
+      raise RecursionError("Infinite Loop Detected")
+    elif pos >= len(tracker):
       return(acc)
     else:
       visited.add(pos)
@@ -45,27 +35,19 @@ def hand_held(tracker, loop=True):
         print('ERROR')
 
 def fix_code(t):
-  pprint.pprint(t)
-  print('\n')
   for i in range(len(t)):
-    print( t[i]['action'])
     if t[i]['action'] == 'jmp':
-      tnew = t.deepcopy()
       t[i]['action'] = 'nop'
-      pprint.pprint(t)
-      print('\n')
-      #try:
-      return(hand_held(t, True))
-      #except RecursionError:
-      t[i]['action'] = 'jmp'
+      try:
+        return(hand_held(t, False))
+      except RecursionError:
+        t[i]['action'] = 'jmp'
     elif t[i]['action'] == 'nop':
       t[i]['action'] = 'jmp'
-      pprint.pprint(t)
-      print('\n')
-#      try:
-      return(hand_held(t, True))
-#      except RecursionError:
-      t[i]['action'] = 'nop'
+      try:
+        return(hand_held(t, False))
+      except RecursionError:
+        t[i]['action'] = 'nop'
 
 print(hand_held(tracking))
-#print(fix_code(tracking))
+print(fix_code(tracking))
