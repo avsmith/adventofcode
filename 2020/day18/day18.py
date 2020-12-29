01e42859c80628609aad67734593b256a2c292f4
Albert Vernon Smith <albertvs@umich.edu>
2020-12-29 07:39:48 +0000

    part2 (override precedence!)

--- 2020/day18/day18.py
+++ 2020/day18/day18.py
@@ -6,61 +6,46 @@ import os, sys
 f = open(os.path.join(sys.path[0], 'input18.txt'))
 input = f.read()
 
-
-thecontent = pyparsing.Word(pyparsing.alphanums) | '*' | '+'
-parens     = pyparsing.nestedExpr( '(', ')')
-
-#res = parens.parseString("(5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)))")
-#res = parens.parseString("(2 * 3 + (4 * 5))")
-res = parens.parseString("(((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2)")
-#res = parens.parseString("(5 + (8 * 3 + 9 + 3 * 4 * 3))")
-print(res.asList())
-
-def recursive_walk(l, curval=0, operand= '+'):
+def recursive_math(l, curval=0, operand= '+'):
   for item in list(l):
     if isinstance(item, pyparsing.ParseResults):
-      returnval = recursive_walk(item, curval=0)
+      returnval = recursive_math(item, curval=0)
       operation = f"{curval} {operand} {returnval}"
       curval = eval(operation)
     else:
       if item == '+':
-#        print('plus')
         operand = '+'
       elif item == '*':
-#        print('times')
         operand = '*'
       else:
         operation = f"{curval} {operand} {item}"
         curval = eval(operation)
   return(curval)
 
-total = 0
+# Set up for part 1
+parens = pyparsing.nestedExpr()
 
-for line in input.splitlines():
-  parsed = parens.parseString('('+line+')')
-  result = recursive_walk(parsed)
-#  print(line, result)
-  total += result
-
-print(total)
-
-from pyparsing import *
-integer = pyparsing_common.signed_integer
-varname = pyparsing_common.identifier 
-arith_expr = infixNotation(integer | varname,
+# Set up parsing for part 2
+# Override precedence!
+integer = pyparsing.pyparsing_common.signed_integer
+arith_expr = pyparsing.infixNotation(integer,
   [
-  ('-', 1, opAssoc.RIGHT),
-  (oneOf('+ -'), 2, opAssoc.LEFT),
-  (oneOf('* /'), 2, opAssoc.LEFT),
+  ('-', 1, pyparsing.opAssoc.RIGHT),
+  (pyparsing.oneOf('+ -'), 2, pyparsing.opAssoc.LEFT),
+  (pyparsing.oneOf('* /'), 2, pyparsing.opAssoc.LEFT),
   ])
 
-arith_expr.runTests('''
-  5+3*6
-  (5+3)*6
-  -2--11
-  ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
-  ''', fullDump=False)
-RESULT = arith_expr.parseString('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2').asList()
-print type(RESULT)
-from pprint import pprint
-pprint(RESULT)
+part1 = 0
+part2 = 0
+
+for line in input.splitlines():
+  parsed1 = parens.parseString('('+line+')')
+  result1 = recursive_math(parsed1)
+  part1 += result1
+  parsed2 = arith_expr.parseString(line)
+  result2 = recursive_math(parsed2)
+  part2 += result2
+
+
+print(part1)
+print(part2)
