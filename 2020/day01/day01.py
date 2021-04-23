1356a952fa7d74e649877530c0600c60baa58ec9
Albert Vernon Smith <albertvs@umich.edu>
2021-04-23 17:39:20 +0000

    Linting fixes

--- 2020/day01/day01.py
+++ 2020/day01/day01.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-import os, sys
+import os
+import sys
 
 f = open(os.path.join(sys.path[0], 'input01.txt'))
 data = f.read()
@@ -9,19 +10,21 @@ datai = [int(x) for x in data.splitlines()]
 
 target = 2020
 
-def report2(l,t):
-  for i in l:
-    for j in l:
-      if i + j== t:
+
+def report2(num, target):
+  for i in num:
+    for j in num:
+      if i + j == target:
         return(i*j)
 
-def report3(l,t):
-  for i in l:
-    for j in l:
-      for k in l:
-        if i + j + k== t:
+
+def report3(num, target):
+  for i in num:
+    for j in num:
+      for k in num:
+        if i + j + k == target:
           return(i*j*k)
-  
+
 
 print("Part 1 Answer:", report2(datai, target))
 print("Part 2 Answer:", report3(datai, target))
