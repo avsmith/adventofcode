ab1011446d827ca14ce306741c789d34a2143592
Albert Vernon Smith <albertvs@umich.edu>
2021-04-25 00:34:41 -0400

    Fix hard path; cleaning

--- 2017/day09.py
+++ 2017/day09.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-f = open('/Users/albert/Documents/Programming/gitwork/adventOfCode/2017/day09.txt','r')
+import os, sys
+
+f = open(os.path.join(sys.path[0], "day09.txt"))
 
 text = f.read()
 
-TESTDATA = '''\
+TESTDATA = """\
 {}
 {{{}}}
 {{},{}}
@@ -13,17 +15,19 @@ TESTDATA = '''\
 {{<ab>},{<ab>},{<ab>},{<ab>}}
 {{<!!>},{<!!>},{<!!>},{<!!>}},
 {{<a!>},{<a!>},{<a!>},{<ab>}}
-'''
+"""
 
 import re
 
+
 def bookmark():
-  return False
+    return False
+
 
 # Match exclamation marks and following
-p_exclaim = re.compile('!.')
+p_exclaim = re.compile("!.")
 # Clean out that text
-cleaned = p_exclaim.sub('',text)
+cleaned = p_exclaim.sub("", text)
 
 
 curscore = 1
@@ -34,21 +38,21 @@ garbage = False
 split_cleaned = list(cleaned)
 
 for i in range(len(split_cleaned)):
-  curchar = split_cleaned[i]
-  if not garbage and curchar == '<':
-    garbage = True
-    continue
-  if garbage and curchar != '>':
-    garbagescore += 1
-    continue
-  if garbage and curchar == '>':
-    garbage = False
-    continue
-  if curchar == '{':
-    runningscore += curscore
-    curscore += 1
-  if curchar == '}':
-    curscore -= 1
+    curchar = split_cleaned[i]
+    if not garbage and curchar == "<":
+        garbage = True
+        continue
+    if garbage and curchar != ">":
+        garbagescore += 1
+        continue
+    if garbage and curchar == ">":
+        garbage = False
+        continue
+    if curchar == "{":
+        runningscore += curscore
+        curscore += 1
+    if curchar == "}":
+        curscore -= 1
 
 print("Answer star 1: {}".format(runningscore))
 print("Answer star 2: {}".format(garbagescore))
