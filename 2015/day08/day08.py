77311faf6fe13f0bbecd43b8b4e59f66ea109c24
Albert Vernon Smith <albertvs@umich.edu>
2021-04-26 10:34:25 -0400

    2015/day08/pt2 solved

--- 2015/day08/day08.py
+++ 2015/day08/day08.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 
-import os, sys
-import numpy as np
-import itertools
+import os
+import sys
 
 f = open(os.path.join(sys.path[0], "input08.txt"))
 input = f.read()
+
+
+def encode_string(s):
+    new = ""
+    for c in s:
+        if c == '"':
+            new += '\\"'
+        elif c == "\\":
+            new += "\\\\"
+        else:
+            new += c
+    return '"' + new + '"'
 
 
 length = 0
@@ -15,9 +26,12 @@ for string in input.splitlines():
     decoded_string = bytes(string[1:-1], "ascii").decode("unicode_escape")
     length += len(string) - len(decoded_string)
     decode2 = bytes(string, "ascii").decode("unicode_escape")
-    encoded_string = string.encode("ascii", "ignore")
-#  print(len(string), len(decoded_string),len(encoded_string))
-#  print(string, decoded_string, encoded_string)
+    encoded_string = string.replace('"', '\\"')
+    encoded_string2 = encode_string(string)
+    length2 += len(encoded_string2) - len(string)
+    #  print(len(string), len(decoded_string),len(encoded_string))
+    print(encoded_string, len(encoded_string), encoded_string2, len(encoded_string2))
 
 
 print(f"Part 1 answer: {length}")
+print(f"Part 2 answer: {length2}")
