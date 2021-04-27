5f23d5d4fe2950b3e86cd39972dc870c4506e865
Albert Vernon Smith <albertvs@umich.edu>
2021-04-27 09:57:58 -0400

    Both parts done

--- 2015/day10/day10.py
+++ 2015/day10/day10.py
@@ -6,18 +6,18 @@ input = "1113222113"
 
 
 def split_count(txt):
-    res = [iters.group(0) for iters in re.finditer(r"(\d)\1*", txt)]
+    splittxt = [iters.group(0) for iters in re.finditer(r"(\d)\1*", txt)]
     ret = ""
-    for x in res:
+    for x in splittxt:
         ret += str(len(x)) + str(x)[0]
     return ret
 
 
 def wrap_split(txt, rounds=40):
-    txt
     for i in range(rounds):
         txt = split_count(txt)
     return txt
 
 
-print(len(wrap_split(input)))
+print("Part 1:", len(wrap_split(input)))
+print("Part 2:", len(wrap_split(input, 50)))
