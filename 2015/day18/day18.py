5a325aef773342f1d0cae60bbf5c927f2597c111
Albert Vernon Smith <albertvs@umich.edu>
2021-04-29 11:52:27 -0400

    completed

--- 2015/day18/day18.py
+++ 2015/day18/day18.py
@@ -16,12 +16,16 @@ f = open(os.path.join(sys.path[0], "input18.txt"))
 input = f.read()
 
 grid = [[1 if i == "#" else 0 for i in row] for row in input.splitlines()]
+grid2 = grid
 
 
-def neighbors(array):
+def neighbors(array, stuck=False):
     nrows = len(array)
     ncols = len(array[0])
     new = [[0 for i in range(ncols)] for j in range(nrows)]
+    if stuck:
+        array = light_corners(array)
+        new = light_corners(new)
     for i in range(nrows):
         for j in range(ncols):
             current = array[i][j]
@@ -37,7 +41,18 @@ def neighbors(array):
     return new
 
 
+def light_corners(array):
+    array[0][0] = 1
+    array[-1][0] = 1
+    array[0][-1] = 1
+    array[-1][-1] = 1
+    return array
+
+
 for i in range(100):
     grid = neighbors(grid)
+for i in range(100):
+    grid2 = neighbors(grid2, stuck=True)
 
 print("Part 1:", np.sum(grid))
+print("Part 2:", np.sum(grid2))
