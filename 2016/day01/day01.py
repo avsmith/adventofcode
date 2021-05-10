b33e4408dc5f55d5e48e565477876500e024a61b
Albert Vernon Smith <albertvs@umich.edu>
2021-05-10 08:58:06 -0400

    prepare for part 2

--- 2016/day01/day01.py
+++ 2016/day01/day01.py
@@ -6,16 +6,22 @@ from collections import defaultdict
 input = "R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2, R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4, L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3, R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2, R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1, L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5, L1, R2, L5, L2, L3, R2, L2"
 
 
-direction = 1
-position = 0 + 0j
-visited = defaultdict()
+moves = input.split(", ")
 
-for instruction in input.split(", "):
-    if instruction[0] == "R":
-        direction *= 1j
-    elif instruction[0] == "L":
-        direction *= -1j
-    distance = int(instruction[1:])
-    position += distance * direction
 
-print("Part 1:", int(abs(position.real) + abs(position.imag)))
+def bunny_path(moves, part2=False):
+    direction = 1
+    position = 0 + 0j
+    visited = defaultdict()
+
+    for instruction in moves:
+        if instruction[0] == "R":
+            direction *= 1j
+        elif instruction[0] == "L":
+            direction *= -1j
+        distance = int(instruction[1:])
+        position += distance * direction
+    return int(abs(position.real) + abs(position.imag))
+
+
+print("Part 1:", bunny_path(moves))
