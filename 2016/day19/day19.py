9c3a3032024fd17f77d82c9e10ae966d062670ca
Albert Vernon Smith <albertvs@umich.edu>
2021-05-20 18:09:06 -0400

    Would work, too slow

--- 2016/day19/day19.py
+++ 2016/day19/day19.py
@@ -15,14 +15,33 @@ def find_elf(num, part2=False):
     presents = [k for k in elves if elves[k] > 0]
     while len(presents) > 1:
         for i in range(len(presents)):
-            if i == len(presents) - 1 and elves[presents[i]] > 0:
-                elves[presents[i]] += elves[presents[0]]
-                elves[presents[0]] = 0
-            elif elves[presents[i]] > 0:
-                elves[presents[i]] += elves[presents[i + 1]]
-                elves[presents[i + 1]] = 0
+            if elves[presents[i]] > 0:
+                if not part2:
+                    if i == len(presents) - 1:
+                        target = presents[0]
+                    else:
+                        target = presents[i + 1]
+                    elves[presents[i]] += elves[target]
+                    elves[target] = 0
+                else:
+                    remaining = [k for k in elves if elves[k] > 0]
+                    if len(remaining) % 1000 == 0 or len(remaining) < 10 == 0:
+                        print(len(remaining))
+                    if len(remaining) > 1:
+                        target = find_across(remaining, presents[i])
+                        elves[presents[i]] += elves[target]
+                        elves[target] = 0
+
         presents = [k for k in elves if elves[k] > 0]
     return presents[0]
 
 
-print("Part 1:", find_elf(input))
+def find_across(list, elf):
+    across = list.index(elf) + len(list) // 2
+    if across >= len(list):
+        across = across - len(list)
+    return list[across]
+
+
+# print("Part 1:", find_elf(input))
+print("Part 2:", find_elf(input, True))
