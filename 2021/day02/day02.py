8c84d2ae15f4adade9b844e7d054f7bc542f3352
Albert Vernon Smith <albertvs@umich.edu>
2021-12-02 14:36:28 +0000

    As function

--- 2021/day02/day02.py
+++ 2021/day02/day02.py
@@ -8,17 +8,18 @@ input = f.read()
 
 data = [x.split() for x in input.splitlines()]
 
-height = 0
-lateral = 0
+
+def move_sub(data, part2=False):
+    depth = 0
+    lateral = 0
+    for move, dist in data:
+        if move == 'up':
+            depth -= int(dist)
+        elif move == 'down':
+            depth += int(dist)
+        elif move == 'forward':
+            lateral += int(dist)
+    return(depth*lateral)
 
 
-for move, dist in data:
-    if move == 'up':
-        height -= int(dist)
-    elif move == 'down':
-        height += int(dist)
-    elif move == 'forward':
-        lateral += int(dist)
-
-print("Part 1:", height*lateral)
-
+print("Part 1:", move_sub(data))
