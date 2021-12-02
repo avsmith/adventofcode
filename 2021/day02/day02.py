984f055c6a8cd5a630ff24b448b0eef9e1ae3531
Albert Vernon Smith <albertvs@umich.edu>
2021-12-02 14:44:48 +0000

    Part 2 done

--- 2021/day02/day02.py
+++ 2021/day02/day02.py
@@ -12,15 +12,27 @@ data = [x.split() for x in input.splitlines()]
 def move_sub(data, part2=False):
     depth = 0
     lateral = 0
+    aim = 0
     for move, dist in data:
         dist = int(dist)
         if move == 'up':
-            depth -= dist
+            if part2:
+                aim -= dist
+            else:
+                depth -= dist
         elif move == 'down':
-            depth += dist
+            if part2:
+                aim += dist
+            else:
+                depth += dist
         elif move == 'forward':
-            lateral += dist
+            if part2:
+                depth += aim*dist
+                lateral += dist
+            else:
+                lateral += dist
     return(depth*lateral)
 
 
 print("Part 1:", move_sub(data))
+print("Part 2:", move_sub(data, True))
