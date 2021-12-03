396097f0c2733a35e98204bc205f6ff117fbd8d1
Albert Vernon Smith <albertvs@umich.edu>
2021-12-03 06:08:35 +0000

    Part 2 works & ugly

--- 2021/day03/day03.py
+++ 2021/day03/day03.py
@@ -14,9 +14,8 @@ def bit_set(list, bit):
     for x in list:
         if x & (1 << bit):
             count += 1
-    if count > len(list)/2:
+    if count >= len(list)/2:
         return True
-
 
 gamma = 0
 epsilon = 0
@@ -28,3 +27,39 @@ for x in range(max(data).bit_length(), 0, -1):
         epsilon ^= 2**(x-1)
 
 print("Part 1:", gamma*epsilon)
+
+
+def oxygen(list, bit):
+    if bit_set(list, bit):
+        new = [x for x in list if x & (1 << bit)]
+    else:
+        new = [x for x in list if not x & (1 << bit)]
+    return(new)
+
+def co2(list, bit):
+    if bit_set(list, bit):
+        new = [x for x in list if not x & (1 << bit)]
+    else:
+        new = [x for x in list if x & (1 << bit)]
+    return(new)
+
+
+
+
+oxy = [int(x,2) for x in input.splitlines()]
+oxy_pos = max(oxy).bit_length() - 1
+
+while len(oxy)>1:
+    oxy = oxygen(oxy,oxy_pos)
+    oxy_pos -= 1
+
+dioxy = [int(x,2) for x in input.splitlines()]
+dioxy_pos = max(dioxy).bit_length() - 1
+
+
+while len(dioxy)>1:
+    dioxy = co2(dioxy,dioxy_pos)
+    dioxy_pos -= 1
+
+
+print(oxy[0]*dioxy[0])
