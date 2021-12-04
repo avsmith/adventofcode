7e23f962683963f1d4fb1bd83bff1a662aa1c66f
Albert Vernon Smith <albertvs@umich.edu>
2021-12-04 07:18:12 +0000

    Cleaning

--- 2021/day04/day04.py
+++ 2021/day04/day04.py
@@ -11,24 +11,17 @@ data = input.splitlines()
 
 moves = [int(x) for x in data[0].split(',')]
 
-#print(len(data))
 boards = []
 
 for i in range(2, len(data), 6):
     board = []
     for row in data[i:i+5]:
         board = np.append(board, np.array(row.split(), dtype=np.int8))
-    boards.append(board.reshape(5,5))
+    boards.append(board.reshape(5, 5))
 
 
 def check_bingo(card, calls):
     status = np.isin(card, calls)
-    # "Diagonals don't count"
-    # RTFM!!!
-    #    if sum(status.diagonal()) == 5:
-    #        return (True)
-    #    if sum(np.fliplr(status).diagonal()) == 5:
-    #        return (True)
     for j in range(5):
         if sum(status[j, :]) == 5:
             return (True)
@@ -37,13 +30,14 @@ def check_bingo(card, calls):
     return (False)
 
 
-def find_winner(boards, moves):
+def find_first(boards, moves):
     for i in range(len(moves)):
         called = moves[0:i+1]
         for board in boards:
             bingo = check_bingo(board, called)
             if bingo:
                 return(board, called)
+
 
 def find_last(boards, moves):
     last = False
@@ -60,7 +54,15 @@ def find_last(boards, moves):
             last = True
         boards = losers
 
-b, m = find_winner(boards, moves)
-print("Part 1:", int(sum(b[np.where(~np.isin(b,m))])*m[-1]))
+
+def board_score(board, moves):
+    remaining_positions = np.where(~np.isin(board, moves))
+    remaining_numbers = board[remaining_positions]
+    last_move = moves[-1]
+    return(int(sum(remaining_numbers) * last_move))
+
+
+b, m = find_first(boards, moves)
+print("Part 1:", board_score(b, m))
 b2, m2 = find_last(boards, moves)
-print("Part 2:", int(sum(b2[np.where(~np.isin(b2,m2))])*m2[-1]))
+print("Part 2:", board_score(b2, m2))
