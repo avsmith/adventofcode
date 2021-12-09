d6c810c9bf41ec081d1962c5317e99d8080dcce1
Albert Vernon Smith <albertvs@umich.edu>
2021-12-09 18:50:47 +0000

    add positions  for neighbors

--- 2021/day09/day09.py
+++ 2021/day09/day09.py
@@ -12,28 +12,33 @@ for line in input.splitlines():
     grid.append([int(x) for x in line])
 
 
+def neighbor_values(grid, i, j):
+    neighbors = []
+    if i != 0 or j != len(grid[i]) - 1 or i != len(grid) - 1 or j != 0:
+        if i != 0:
+            neighbors.append([i - 1, j, grid[i - 1][j]])
+        if j != len(grid[i]) - 1:
+            neighbors.append([i, j + 1, grid[i][j + 1]])
+        if i != len(grid) - 1:
+            neighbors.append([i + 1, j, grid[i + 1][j]])
+        if j != 0:
+            neighbors.append([i, j - 1, grid[i][j - 1]])
+    else:
+        neighbors = [
+            [i - 1, j, grid[i - 1][j]],
+            [i, j + 1, grid[i][j + 1]],
+            [i + 1, j, grid[i + 1][j]],
+            [i, j - 1, grid[i][j - 1]],
+        ]
+    return neighbors
+
+
 def low_points(grid):
     low = []
     for i in range(len(grid)):
         for j, value in enumerate(grid[i]):
-            neighbors = []
-            if i != 0 or j != len(grid[i]) - 1 or i != len(grid) - 1 or j != 0:
-                if i != 0:
-                    neighbors.append(grid[i - 1][j])
-                if j != len(grid[i]) - 1:
-                    neighbors.append(grid[i][j + 1])
-                if i != len(grid) - 1:
-                    neighbors.append(grid[i + 1][j])
-                if j != 0:
-                    neighbors.append(grid[i][j - 1])
-            else:
-                neighbors = [
-                    grid[i - 1][j],
-                    grid[i][j + 1],
-                    grid[i + 1][j],
-                    grid[i][j - 1],
-                ]
-            test = [value < x for x in neighbors]
+            neighbors = neighbor_values(grid, i, j)
+            test = [value < x[2] for x in neighbors]
             if all(test):
                 low.append([i, j])
     return low
