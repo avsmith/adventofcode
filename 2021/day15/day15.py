445a992b2097ef39a8adeefe95ce6ab7d0cec1c9
Albert Vernon Smith <albertvs@umich.edu>
2021-12-18 01:22:43 +0000

    Solved, thnkx to networkx

--- 2021/day15/day15.py
+++ 2021/day15/day15.py
@@ -4,6 +4,7 @@ import sys
 import os
 
 import numpy as np
+import networkx as nx
 
 f = open(os.path.join(sys.path[0], "input15.txt"))
 input = f.read()
@@ -14,18 +15,51 @@ grid = []
 for line in input.splitlines():
     grid.append([int(x) for x in line])
 
-def minCost(cost):
-    mincost = np.zeros_like(cost, dtype= 'i')
-    cost = np.array(cost)
-#    print(mincost)
-    for _ in range(3):
-        for i in range(1,len(cost)):
-            mincost[0,i] = mincost[0,i-1] + cost[0,i]
-        for i in range(1, len(cost[0])):
-            mincost[i,0] = mincost[i-1,0] + cost[i,0]
-        for i in range(1, len(cost[0])):
-            for j in range(1, len(cost)):
-                mincost[i,j] = min(mincost[i-1,j], mincost[i,j-1]) + cost[i,j]
-    return mincost[-1][-1]
 
-print(minCost(grid))
+def make_graph(grid):
+    TmpG = nx.DiGraph()
+
+    for ix, iy in np.ndindex(grid.shape):
+        if ix > 0:
+            TmpG.add_edge((ix - 1, iy), (ix, iy), weight=grid[(ix, iy)])
+
+        if ix < grid.shape[0] - 1:
+            TmpG.add_edge((ix + 1, iy), (ix, iy), weight=grid[(ix, iy)])
+
+        if iy > 0:
+            TmpG.add_edge((ix, iy - 1), (ix, iy), weight=grid[(ix, iy)])
+
+        if iy < grid.shape[1] - 1:
+            TmpG.add_edge((ix, iy + 1), (ix, iy), weight=grid[(ix, iy)])
+    return TmpG
+
+
+def roll_grid(grid, delta):
+    newgrid = np.where(grid + delta > 9, grid + delta - 9, grid + delta)
+    return newgrid
+
+
+def expand_grid(grid, expand=1):
+    horizontal_grid = np.copy(grid)
+    for d in range(1, expand):
+        horizontal_grid = np.append(horizontal_grid, roll_grid(grid, d), axis=1)
+    vertical_grid = np.copy(horizontal_grid)
+    for d in range(1, expand):
+        vertical_grid = np.append(vertical_grid, roll_grid(horizontal_grid, d), axis=0)
+    return vertical_grid
+
+
+grid = np.array(grid)
+
+G1 = make_graph(grid)
+print(
+    "Part 1:",
+    nx.dijkstra_path_length(G1, (0, 0), (grid.shape[0] - 1, grid.shape[1] - 1)),
+)
+
+grid5 = expand_grid(grid, 5)
+G5 = make_graph(grid5)
+print(
+    "Part 2:",
+    nx.dijkstra_path_length(G5, (0, 0), (grid5.shape[0] - 1, grid5.shape[1] - 1)),
+)
