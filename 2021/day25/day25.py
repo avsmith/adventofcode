288a9c2e68d86c945df920bacd22e4877a01e62f
Albert Vernon Smith <albertvs@umich.edu>
2021-12-26 00:07:04 +0000

    Clean, remove integer array

--- 2021/day25/day25.py
+++ 2021/day25/day25.py
@@ -5,75 +5,58 @@ import os
 
 import numpy as np
 
-test = """v...>>.vv>
-.vv>>.vv..
->>.>v>...v
->>v>>.>.v.
-v>v.vv.v..
->.>>..v...
-.vv..>.>v.
-v.v..>>v.v
-....v..v.>
-"""
-
 f = open(os.path.join(sys.path[0], "input25.txt"))
 input = f.read()
 
 grid = []
 
 for line in input.splitlines():
-    arr = []
-    for x in line:
-        if x == "v":
-            arr.append(1)
-        elif x == ">":
-            arr.append(-1)
-        else:
-            arr.append(0)
+    grid.append([x for x in line])
 
-    grid.append(arr)
 grid = np.array(grid)
 
-
+# For debugging, print the grid
 def print_grid(grid):
     header = ""
     for c in range(len(grid[0])):
         header += str(c % 10)
     print(header)
     for line in grid:
-        string = ""
-        for c in line:
-            if c == -1:
-                string += ">"
-            elif c == 1:
-                string += "v"
-            else:
-                string += "."
-        print(string)
+        print("".join(line))
     print("\n")
 
 
 def make_move_right(array):
+    # Create shifted array to find empty slots
     shiftl = np.roll(array, -1, axis=1)
-    moved = np.roll(np.where((shiftl == 0) & (array == -1), -1, 0), 1, axis=1)
-    unmoved = np.where((shiftl != 0) & (array == -1), -1, 0)
-    newright = np.where((unmoved == -1) | (moved == -1), -1, 0)
-    final = np.where(array == 1, 1, newright)
+    # Find positions for moving ">" and hold these
+    moved = np.roll(np.where((shiftl == ".") & (array == ">"), ">", "."), 1, axis=1)
+    # Find positions that can't move ">"
+    unmoved = np.where((shiftl != ".") & (array == ">"), ">", ".")
+    # Combine the moved and unmoved ">"
+    newright = np.where((unmoved == ">") | (moved == ">"), ">", ".")
+    # Take all ">" positions, otherwise the original
+    final = np.where(array == "v", "v", newright)
     return final
 
 
 def make_move_down(array):
+    # Create shifted array to find empty slots
     shiftu = np.roll(array, -1, axis=0)
-    moved = np.roll(np.where((shiftu == 0) & (array == 1), 1, 0), 1, axis=0)
-    unmoved = np.where((shiftu != 0) & (array == 1), 1, 0)
-    newdown = np.where((unmoved == 1) | (moved == 1), 1, 0)
-    final = np.where(array == -1, -1, newdown)
+    # Find positions for moving "v" and hold these
+    moved = np.roll(np.where((shiftu == ".") & (array == "v"), "v", "."), 1, axis=0)
+    # Find positions that can't move "v"
+    unmoved = np.where((shiftu != ".") & (array == "v"), "v", ".")
+    # Combine the moved and unmoved "v"
+    newdown = np.where((unmoved == "v") | (moved == "v"), "v", ".")
+    # Take all "v" positions, otherwise the original
+    final = np.where(array == ">", ">", newdown)
     return final
 
 
 def find_stuck(array):
     count = 0
-    while count < 1000:
+    while True:
         count += 1
         newarray = make_move_down(make_move_right(array))
         if np.array_equal(array, newarray):
