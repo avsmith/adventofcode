2f0e9f8bde0dc4fd8ca0a1651a1880bd4d6006cd
Albert Vernon Smith <albertvs@umich.edu>
2022-12-06 20:50:01 +0000

    2022/day06 done

---
+++ 2022/day06/day06.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+
+import os
+import sys
+
+f = open(os.path.join(sys.path[0], "input06.txt"))
+input = f.read()
+
+
+def uniq_chars(word):
+    chars = [*word]
+    return len(chars) == len(set(chars))
+
+
+def marker(word, length=4):
+    for i in range(len(input) - length + 1):
+        test = input[i : i + length]
+        if uniq_chars(test):
+            return i + length
+    return -1
+
+
+print("Part1:", marker(input))
+print("Part2:", marker(input, 14))
