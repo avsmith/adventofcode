95055ba56acb2ecd570459c0bc3e316f9ee33d1c
Albert Vernon Smith <albertvs@umich.edu>
2022-12-06 18:18:07 +0000

    2022/day05 complete

--- 2022/day05/day05.py
+++ 2022/day05/day05.py
@@ -11,8 +11,6 @@ setup, moves = input.split("\n\n")
 setup = [l for l in setup.splitlines()]
 moves = [l for l in moves.splitlines()]
 
-elements = len(setup[-1].split())
-
 
 class Stacks:
     def __init__(self, setup, part2=False):
@@ -26,37 +24,21 @@ class Stacks:
                 if char != " ":
                     self.stacks[i].append(char)
 
-    #    def stacks(self):
-    #        return self.stacks
-
-    def move(self, number, source, dest, new=False):
+    def move(self, number, source, dest):
         for i in range(number):
-            if new:
+            if self.part2:
                 pos = i
             else:
                 pos = 0
             self.stacks[dest].insert(pos, self.stacks[source].popleft())
 
     def tops(self):
-        x = "".join([x[0] for x in self.stacks])
-
-        return x
-
-
-stack = Stacks(setup)
-
-# print(s.tops())
-# print(s.stacks)
+        tops = "".join([x[0] for x in self.stacks])
+        return tops
 
 
-tracking = [deque() for _ in range(elements)]
-
-for input in setup[0:-1]:
-    for i in range(elements):
-        pos = i * 4 + 1
-        char = input[pos]
-        if char != " ":
-            tracking[i].append(char)
+part1 = Stacks(setup)
+part2 = Stacks(setup, True)
 
 
 for move in moves:
@@ -64,11 +46,11 @@ for move in moves:
     number = int(splits[1])
     source = int(splits[3]) - 1
     dest = int(splits[5]) - 1
-    for i in range(number):
-        tracking[dest].insert(i, tracking[source].popleft())
+    part1.move(number, source, dest)
+    part2.move(number, source, dest)
 
-
-print("Part1:", "".join([x[0] for x in tracking]))
+print("Part1:", part1.tops())
+print("Part2:", part2.tops())
 
 # Part1: SBPQRSCDF
 # Part2: RGLVRCQSB
