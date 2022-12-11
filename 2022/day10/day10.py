ab14aa377585d7ba2faf2ebab57a18c08457e961
Albert Vernon Smith <albertvs@umich.edu>
2022-12-11 16:05:19 +0000

    day10 completed (with some inelegance)

--- 2022/day10/day10.py
+++ 2022/day10/day10.py
@@ -11,27 +11,38 @@ result = 0
 
 step = 1
 
+terminal = ["" for _ in range(6)]
+
 for line in input.splitlines():
     items = line.split()
     instruction = items[0]
     if len(items) == 2:
         amount = int(items[1])
         for _ in range(2):
-
+            row = (step - 1) // 40
             if step in (20, 60, 100, 140, 180, 220):
-                #                print(step, value, step * value)
                 result += step * value
+            if step - 40 * row in range(value, value + 3):
+                terminal[row] += "#"
+            else:
+                terminal[row] += "."
             step += 1
-
     else:
         amount = 0
-
+        # This bit nearly duplicated that above
+        # Should be turned into a function for both
+        row = (step - 1) // 40
         if step in (20, 60, 100, 140, 180, 220):
-            #            print(step, value, step * value)
             result += step * value
+        if step - 40 * row in range(value, value + 3):
+            terminal[row] += "#"
+        else:
+            terminal[row] += "."
         step += 1
 
     value += amount
 
-
-print(result)
+print("Part1:", result)
+# Need to read this to letters
+print("Part2:")
+print("\n".join(terminal))
