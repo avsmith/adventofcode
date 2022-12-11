0df6b6a2cbe99ab5bcf518211aba9dc01402f175
Albert Vernon Smith <albertvs@umich.edu>
2022-12-11 02:35:34 +0000

    part 2 solved; variable snakelength

--- 2022/day09/day09.py
+++ 2022/day09/day09.py
@@ -18,76 +18,86 @@ R 2
 
 
 class Snake:
-    def __init__(self):
-        self.head = {"x": 0, "y": 0}
-        self.tail = {"x": 0, "y": 0}
-        #        self.headvisited = set()
+    def __init__(self, length):
+        self.snake = [[0, 0] for x in range(length)]
+        self.length = length
         self.tailstops = set()
-        self.recordtail(0, 0)
+        self.recordtail()
+
+    def recordtail(self):
+        self.tailstops.add((self.snake[-1][0], self.snake[-1][1]))
 
     def moveup(self, num):
         for _ in range(num):
-            self.head["y"] += 1
-            self.movetail()
+            self.snake[0][1] += 1
+            self.movesnake()
 
     def movedown(self, num):
         for _ in range(num):
-            self.head["y"] -= 1
-            self.movetail()
+            self.snake[0][1] -= 1
+            self.movesnake()
 
     def moveright(self, num):
         for _ in range(num):
-            self.head["x"] += 1
-            self.movetail()
+            self.snake[0][0] += 1
+            self.movesnake()
 
     def moveleft(self, num):
         for _ in range(num):
-            self.head["x"] -= 1
-            self.movetail()
+            self.snake[0][0] -= 1
+            self.movesnake()
 
-    def movetail(self):
-        if (
-            self.head["x"] == self.tail["x"]
-            and abs(self.head["y"] - self.tail["y"]) > 1
-        ):
-            self.tail["y"] += (self.head["y"] - self.tail["y"]) / abs(
-                self.head["y"] - self.tail["y"]
-            )
-        elif (
-            self.head["y"] == self.tail["y"]
-            and abs(self.head["x"] - self.tail["x"]) > 1
-        ):
-            self.tail["x"] += (self.head["x"] - self.tail["x"]) / abs(
-                self.head["x"] - self.tail["x"]
-            )
-        elif (
-            abs(self.head["x"] - self.tail["x"]) > 1
-            or abs(self.head["y"] - self.tail["y"]) > 1
-        ):
-            self.tail["x"] += (self.head["x"] - self.tail["x"]) / abs(
-                self.head["x"] - self.tail["x"]
-            )
-            self.tail["y"] += (self.head["y"] - self.tail["y"]) / abs(
-                self.head["y"] - self.tail["y"]
-            )
-        self.recordtail(int(self.tail["x"]), int(self.tail["y"]))
+    def movesnake(self):
+        for i in range(1, len(self.snake)):
+            if (
+                self.snake[i - 1][0] == self.snake[i][0]
+                and abs(self.snake[i - 1][1] - self.snake[i][1]) > 1
+            ):
+                self.snake[i][1] += int(
+                    (self.snake[i - 1][1] - self.snake[i][1])
+                    / abs(self.snake[i - 1][1] - self.snake[i][1])
+                )
+            elif (
+                self.snake[i - 1][1] == self.snake[i][1]
+                and abs(self.snake[i - 1][0] - self.snake[i][0]) > 1
+            ):
+                self.snake[i][0] += int(
+                    (self.snake[i - i][0] - self.snake[i][0])
+                    / abs(self.snake[i - i][0] - self.snake[i][0])
+                )
+            elif (
+                abs(self.snake[i - 1][0] - self.snake[i][0]) > 1
+                or abs(self.snake[i - 1][1] - self.snake[i][1]) > 1
+            ):
+                self.snake[i][1] += int(
+                    (self.snake[i - 1][1] - self.snake[i][1])
+                    / abs(self.snake[i - 1][1] - self.snake[i][1])
+                )
+                self.snake[i][0] += int(
+                    (self.snake[i - 1][0] - self.snake[i][0])
+                    / abs(self.snake[i - 1][0] - self.snake[i][0])
+                )
+            self.recordtail()
 
-    def recordtail(self, x, y):
-        self.tailstops.add((x, y))
 
-
-part1 = Snake()
+part1 = Snake(2)
+part2 = Snake(10)
 
 for line in input.splitlines():
     dir, n = line.split()
     n = int(n)
     if dir == "U":
         part1.moveup(n)
+        part2.moveup(n)
     elif dir == "D":
         part1.movedown(n)
+        part2.movedown(n)
     elif dir == "R":
         part1.moveright(n)
+        part2.moveright(n)
     elif dir == "L":
         part1.moveleft(n)
+        part2.moveleft(n)
 
-print(len(part1.tailstops))
+print("Part1:", len(part1.tailstops))
+print("Part2:", len(part2.tailstops))
