19ba39f92056111a2ba4b50dfc00865c310ec635
Albert Vernon Smith <albertvs@umich.edu>
2022-12-11 23:27:14 +0000

    Part2 done (using duct tape)

--- 2022/day11/day11.py
+++ 2022/day11/day11.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import numpy as np
 
 f = open(os.path.join(sys.path[0], "input11.txt"))
 input = f.read()
@@ -37,20 +38,27 @@ Monkey 3:
 
 
 class Monkey:
-    def __init__(self, name, items, operation, divisor, truedest, falsedest):
+    def __init__(
+        self, name, items, operation, divisor, truedest, falsedest, part2=False
+    ):
         self.name = name
         self.items = items
         self.operation = operation
         self.divisor = divisor
         self.truedest = truedest
         self.falsedest = falsedest
+        self.part2 = part2
+        if not part2:
+            self.worry_fix = 3
+        else:
+            self.worry_fix = 1
         self.inspected = 0
 
     def inspect(self):
         while self.items:
             old = self.items.pop(0)
             new = eval(self.operation)
-            new //= 3
+            new //= self.worry_fix
             if new % self.divisor == 0:
                 recipient = next((m for m in monkeys if m.name == self.truedest), None)
             else:
@@ -59,6 +67,8 @@ class Monkey:
             recipient.catch(new)
 
     def catch(self, worry):
+        if self.part2:
+            worry = worry % self.lcm
         self.items.append(worry)
 
     def inspections(self):
@@ -77,8 +87,11 @@ Inspected: {self.inspected}
 """
         return string
 
+    def setlcm(self, lcm):
+        self.lcm = lcm
 
-def MakeMonkey(text):
+
+def MakeMonkey(text, part2=False):
     for line in text.splitlines():
         if "Monkey" in line:
             name = int(line[7:-1])
@@ -93,12 +106,11 @@ def MakeMonkey(text):
             truedest = int(line[29:])
         elif "If false" in line:
             falsedest = int(line[30:])
-    monkey = Monkey(name, items, operation, divisor, truedest, falsedest)
+    monkey = Monkey(name, items, operation, divisor, truedest, falsedest, part2)
     return monkey
 
 
 monkeys = []
-
 
 for text in input.split("\n\n"):
     monkey = MakeMonkey(text)
@@ -113,3 +125,26 @@ for _ in range(20):
 
 inspections = sorted([x.inspected for x in monkeys], reverse=True)
 print("Part1:", inspections[0] * inspections[1])
+
+
+monkeys = []
+
+for text in input.split("\n\n"):
+    monkey = MakeMonkey(text, True)
+    monkeys.append(monkey)
+#    print(monkey)
+
+lcm = np.prod([x.divisor for x in monkeys])
+
+for m in monkeys:
+    m.setlcm(lcm)
+
+
+for _ in range(10000):
+    for m in range(len(monkeys)):
+        monkey = next((x for x in monkeys if x.name == m), None)
+        monkey.inspect()
+
+
+inspections = sorted([x.inspected for x in monkeys], reverse=True)
+print("Part2:", inspections[0] * inspections[1])
