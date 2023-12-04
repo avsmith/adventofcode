7849060f3dc1feda0b2b2e3429386e4ced7969cb
Albert Smith <albert@Alberts-Mac-mini.local>
2023-12-04 02:46:40 +0000

    part2, finally

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -2,15 +2,38 @@
 
 import os
 import sys
+import re
 
+# For part 2, crazy substitute due to overlapping patterns
+number_dict = {
+	'one': 'o1e',
+	'two': 't2o',
+	'three': 't3e',
+	'four' : 'f4r',
+	'five': 'f5e',
+	'six': 's6x',
+	'seven': 's7n',
+	'eight': 'e8t',
+	'nine': 'n9e',
+}
 
+def replace_words(s):
+	for k,v in number_dict.items():
+		s = s.replace(k,v)
+	return(s)
+	
 def get_digits(text, substitute = False):
+	if substitute:
+		text = replace_words(text)
 	nums = ''.join(c for c in text if c.isdigit())
-	return int(nums[0] + nums[-1])
+	reduced = nums[0] + nums[-1]
+	return(int(reduced))
 
 f = open(os.path.join(sys.path[0], "input01.txt"))
 input = f.read()
 
 part1 = [get_digits(x, False) for x in input.splitlines()]
+part2 = [get_digits(x, True) for x in input.splitlines()]
 
 print("Part 1:", sum(part1))
+print("Part 2:", sum(part2))
