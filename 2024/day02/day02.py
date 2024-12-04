31120fd8d057a3e2cc50c917e2f103757f229950
Albert Vernon Smith <albertvs@umich.edu>
2024-12-04 07:33:36 -0500

    Day 2 done

--- 2024/day02/day02.py
+++ 2024/day02/day02.py
@@ -9,8 +9,7 @@ input = f.read()
 good = 0
 
 
-def safe(report):
-    d = [int(ele) for ele in report.split()]
+def safe(d):
     inc = all(i < j for i, j in zip(d, d[1:]))
     dec = all(i > j for i, j in zip(d, d[1:]))
     if inc or dec:
@@ -26,6 +25,20 @@ def safe(report):
         return False
 
 
+def checkReports(l, part2=False):
+    d = [int(ele) for ele in l.split()]
+    if part2:
+        for i in range(len(d)):
+            subreport = list(d)
+            del subreport[i]
+            subsafe = safe(subreport)
+            if subsafe:
+                return subsafe
+
+    else:
+        return safe(d)
+
+
 def maxDiff(a):
     diff_list = []
 
@@ -35,9 +48,15 @@ def maxDiff(a):
     return max(diff_list)
 
 
-good = 0
+good1 = 0
+good2 = 0
 for line in input.splitlines():
-    if safe(line):
-        good += 1
+    if checkReports(line):
+        good1 += 1
+        good2 += 1
+    elif checkReports(line, True):
+        good2 += 1
 
-print(good)
+
+print(f"Part 1: {good1}")
+print(f"Part 1: {good2}")
