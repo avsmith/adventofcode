2c5cd0f0374bf04984f7cf8aeb3b50ad5d45a5b4
Albert Vernon Smith <albertvs@umich.edu>
2024-12-24 16:34:10 -0500

    Day 3 done

--- 2024/day03/day03.py
+++ 2024/day03/day03.py
@@ -7,13 +7,27 @@ import re
 f = open(os.path.join(sys.path[0], "input03.txt"))
 input = f.read()
 
-test = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-
 matches = re.findall(r"mul\(\d+,\d+\)", input)
 
-tot = 0
-for m in matches:
-    i, j = re.findall(r"\d+", m)
-    tot += int(i) * int(j)
+total1 = 0
 
-print(tot)
+for a, b in re.findall(r"mul\((\d+),(\d+)\)", input):
+    total1 += int(a) * int(b)
+
+print(f"Part 1: {total1}")
+
+do = r"do\(\)"
+dont = r"don't\(\)"
+mul = r"mul\((\d+),(\d+)\)"
+total2 = 0
+enabled = True
+for x in re.finditer(f'{do}|{dont}|{mul}', input):
+    if re.fullmatch(do, x.group()):
+        enabled = True
+    elif re.fullmatch(dont, x.group()):
+        enabled = False
+    elif enabled:
+        total2 += int(x.group(1)) * int(x.group(2))
+
+print(f"Part 2: {total2}")
+
