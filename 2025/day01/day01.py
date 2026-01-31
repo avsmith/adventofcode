3e0b1bb0329e155840b460d2e573bfe5a0a2fb9d
Albert Vernon Smith <albert@alberts-macbook-air.local>
2026-01-31 17:30:25 +0000

    Day 01 finished

--- 2025/day01/day01.py
+++ 2025/day01/day01.py
@@ -19,9 +19,9 @@ f = open(os.path.join(sys.path[0], "input.txt"))
 data = f.read()
 
 input = data.splitlines()
-bl
-times = 0
+
 position = 50
+part1times = 0
 
 for c in input:
     direction = c[0]
@@ -31,12 +31,34 @@ for c in input:
         position -= value
     elif direction == "R":
         position += value
-
-
     position %= 100
 
+    # Count times at position zero
     if position == 0:
-        times += 1
+        part1times += 1
 
+position = 50
+part2times = 0
 
-print(times)
+for line in input:
+
+    direction = line[0]
+    value = int(line[1:])
+
+    # Split to count full rotations and leave remainder
+    full, partial = divmod(value, 100)
+    part2times += full
+
+    delta = -partial if direction == "L" else partial
+    next_position = position + delta
+
+    if position != 0:
+        if direction == "L" and next_position <= 0:
+            part2times += 1
+        elif direction == "R" and next_position >= 100:
+            part2times += 1
+
+    position = next_position % 100
+
+print(f"Part 1: {part1times}")
+print(f"Part 2: {part2times}")
