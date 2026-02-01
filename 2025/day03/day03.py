2c33290d034cd08972ec08cf2d0ddbbe2f7f0eba
Albert Vernon Smith <albert@alberts-macbook-air.local>
2026-02-01 13:08:03 +0000

    recursive refactoring

--- 2025/day03/day03.py
+++ 2025/day03/day03.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 import numpy as np
+import itertools
 
 testdata = """987654321111111
 811111111111119
@@ -14,19 +15,24 @@ testdata = """987654321111111
 f = open(os.path.join(sys.path[0], "input03.txt"))
 data = f.read()
 
-input = data.splitlines()
+input = testdata.splitlines()
+        
+def find_digit(input, size=2, output=''):
+    ints = [int(d) for d in input]
+    end = size-len(output)-1
+    if end > 0:
+        digpos = np.argmax(ints[:-end])
+    else:
+        digpos = np.argmax(ints)
+    output += str(ints[digpos])
+    if len(output) == size:
+        return output
+    else:
+        remainder = input[(digpos+1):]
+        return(find_digit(remainder, size, output))
 
-# [int(d) for d in str(s)]
-# a = [12, 4, 3, 7, 8, 10, 22]
-# print(np.argmax(a))
-
-tot = 0
-
+part1 = 0
 for x in input:
-    ints = [int(d) for d in x]
-    tenspos = np.argmax(ints[:-1])
-    digpos = np.argmax(ints[(tenspos + 1) :]) + tenspos + 1
-    value = 10 * ints[tenspos] + ints[digpos]
-    tot += value
-
-print(tot)
+    part1 += int(find_digit(x))
+print(part1)
+           
