dfcd984f991f7b48f9f64711654838d767e37435
Albert Vernon Smith <albert@alberts-macbook-air.local>
2026-02-01 19:14:26 +0000

    More pythonic

--- 2025/day05/day05.py
+++ 2025/day05/day05.py
@@ -3,26 +3,26 @@
 from pathlib import Path
 import portion as p
 
-text = Path("input05.txt").read_text()
+inputtext = Path("input05.txt").read_text()
+
+def parse_intervals(text):
+	interval_set = p.empty()
+	for left, right in (map(int, l.split("-")) for l in text.splitlines()):
+		interval_set |= p.closed(left, right)
+	return interval_set
 
 
-interval_text, value_text = text.split("\n\n")
+def count_values(interval_set, values):
+	return sum(int(v) in interval_set for v in values.splitlines())
 
-interval_set = p.empty()
 
-for line in interval_text.splitlines():
-	left, right = map(int, line.split("-"))
-	interval_set |= p.closed(left, right)
+def interval_size(interval_set):
+	return sum(i.upper - i.lower + 1 for i in interval_set)
 
-count_values_in_intervals = 0
-for v in map(int, value_text.splitlines()):
-	if v in interval_set:
-		count_values_in_intervals += 1
 
-print(count_values_in_intervals)
+interval_text, value_text = inputtext.split("\n\n")
 
-interval_size = 0
-for interval in interval_set:
-	interval_size += interval.upper - interval.lower + 1
+interval_set = parse_intervals(interval_text)
 
-print(interval_size)
+print(count_values(interval_set, value_text))
+print(interval_size(interval_set))
