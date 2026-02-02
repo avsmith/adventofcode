e61ccd35872600601faa6fcadf8651a5126c7ff2
Albert Vernon Smith <albert@alberts-macbook-air.local>
2026-02-02 00:46:46 +0000

    ugly, working part2

--- 2025/day06/day06.py
+++ 2025/day06/day06.py
@@ -29,4 +29,31 @@ for form in transposed_matrix:
 	else:
 		value += sum(form[0:-1])
 
-print(value)
+print("Part1:",value)
+
+m = inputtext.splitlines()
+
+indices = [i for i, c in enumerate(m[-1]) if c == "*" or c == "+"] 
+
+indices.append(len(m[-1])+1)
+digits = len(m)
+full_nums = []
+
+for i in range(len(indices[:-1])):
+	positions = list(range(indices[i+1]-2, indices[i]-1,-1))
+	nums = [''] * (indices[i+1]-indices[i]-1)
+	for idx, p in enumerate(positions):
+		for j in range(len(m)-1):
+			nums[idx] += m[j][p]
+	nums = [int(x) if x else 0 for x in nums]
+	nums.append(m[-1][indices[i]])
+	full_nums.append(nums)
+
+value2 = 0
+for form in full_nums:
+	if form[-1] == '*':
+		value2 += math.prod(form[0:-1])
+	else:
+		value2 += sum(form[0:-1])
+print("Part2:", value2)
+
